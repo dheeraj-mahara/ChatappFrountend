0d295faea40382e96002db3c3d3383b15bc55e8d
dheeraj <mehradheeraj314@gmail.com>
2026-06-08 10:47:17 +0530

    Add sitemap

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import sitemap from "vite-plugin-sitemap";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/'
-})
+  plugins: [
+    react(),
+    sitemap({
+      hostname: "https://chat-vibe-theta.vercel.app",
+    }),
+  ],
+  base: "/",
+});
